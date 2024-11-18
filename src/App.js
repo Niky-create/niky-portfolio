750c3054459e9b462c107df26fa023b04d85b5a1
Niky-create <dvorakovanikola4444@gmail.com>
2024-11-18 19:27:08 +0100

    Fix homepage routing and deploy settings

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/niky-portfolio">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
